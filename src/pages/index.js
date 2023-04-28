19f9d4a4cdfdad3c839adfd52c6c8ccff28c072f
Bamidele Ajibola <107488257+Alphawga@users.noreply.github.com>
2023-04-28 15:58:28 +0100

    Update index.js
    Updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,8 +33,7 @@ export default function Home() {
                 className={styles.vercelLogo}
                 width={100}
                 height={24}
-                priority
-              />
+                priority />
             </a>
           </div>
         </div>
